9dccb71cffa3b8b4450a3944d07a08738123f223
trainmaniak <xhandl@fi.muni.cz>
2018-02-13 10:11:06 +0100

    added win support

---
+++ app-linux.py
@@ -0,0 +1,6 @@
+#!venv-linux/bin/python3
+
+from view import *
+
+if __name__ == "__main__":
+    launch()
---
+++ app-win.py
@@ -0,0 +1,6 @@
+#!venv-win\Scripts\pythonw.exe
+
+from view import *
+
+if __name__ == "__main__":
+    launch()
